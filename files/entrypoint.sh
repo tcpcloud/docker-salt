fdb0ff99d63d762d9a94a3cf81e790e7888aaf0d
Filip Pytloun <filip@pytloun.cz>
2016-05-20 12:37:14 +0200

    Fix sed in entrypoint

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sed -i s,postfix\.server,$(hostname -f) /etc/postfix/main.cf
-sed -i s,\$POSTFIX_ORIGIN,$(hostname -d) /etc/postfix/main.cf
+sed -i s,postfix.server,$(hostname -f),g /etc/postfix/main.cf
+sed -i s,\$POSTFIX_ORIGIN,$(hostname -d),g /etc/postfix/main.cf
 
 /usr/sbin/postfix start
