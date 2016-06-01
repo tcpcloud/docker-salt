a8ac15f17e3bfa3af7110f36957ebd52daa69458
Filip Pytloun <filip@pytloun.cz>
2016-06-01 19:25:08 +0200

    Fix wrong syntax

--- build.sh
+++ build.sh
@@ -37,7 +37,7 @@ wait
 echo
 
 for log_file in log/*.log; do
-    if [[ $(grep "Successfully built " $log_file) ]]; then
+    if [ ! -z $(grep "Successfully built " $log_file 2>/dev/null) ]; then
         echo "== Build of $(basename $log_file .log) failed" 1>&2
         RETVAL=1
     fi
