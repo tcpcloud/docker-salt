458a751e085dacc7a715d501b8a7747e5b1e5fd6
Filip Pytloun <filip@pytloun.cz>
2016-06-01 14:08:15 +0200

    Enhance build.sh

--- build.sh
+++ build.sh
@@ -1,17 +1,21 @@
 #!/bin/bash -e
 
 TAG_PREFIX=tcpcloud
-BUILD_PATH=${1:-"salt-base.dockerfile services"}
+BUILD_PATH=${*:-"salt-base.dockerfile services"}
 SLEEP_TIME=${SLEEP_TIME:-3}
+BUILD_ARGS=${BUILD_ARGS:-""}
 MAX_JOBS=${JOBS:-1}
 
 JOBS=0
+RETVAL=0
 
 build_image() {
     name=$(echo $(basename $1 .dockerfile) | sed 's,\.,-,g')
     echo "== Building $name"
-    docker build --no-cache --rm=true -t $TAG_PREFIX/$name -f $1 .
+    docker build --no-cache --rm=true -t $BUILD_ARGS $TAG_PREFIX/$name -f $1 . 2>&1 | tee log/${name}.log
 }
+
+[ ! -d log ] && mkdir log || rm -f log/*.log
 
 find $BUILD_PATH -name "*.dockerfile" | while read service; do
     if [ "$service" == "salt-base.dockerfile" ]; then
@@ -25,3 +29,14 @@ find $BUILD_PATH -name "*.dockerfile" | while read service; do
         JOBS=$[ $JOBS + 1 ]
     fi
 done
+
+wait
+
+for log_file in log/*.log; do
+    grep "Successfully built " $log_file >/dev/null || (
+        echo "== Build of $(basename $log_file .log) failed" 1>&2;
+        RETVAL=1
+    )
+done
+
+exit $RETVAL
