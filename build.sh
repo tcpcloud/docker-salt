c8eac3a9c52581d36093906a2e24a8eaa66fda5b
Filip Pytloun <filip@pytloun.cz>
2016-06-02 09:50:42 +0200

    Enhance parallel build

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ SLEEP_TIME=${SLEEP_TIME:-3}
 BUILD_ARGS=${BUILD_ARGS:-""}
 MAX_JOBS=${JOBS:-1}
 
-JOBS=0
+JOBS=()
 RETVAL=0
 
 build_image() {
@@ -17,26 +17,44 @@ build_image() {
     stdbuf -oL -eL docker build --no-cache --rm=true -t $TAG_PREFIX/$name $BUILD_ARGS -f $1 . 2>&1 | stdbuf -oL -eL tee log/${name}.log
 }
 
+wait_jobs() {
+    echo "== Waiting for jobs: ${JOBS[@]}"
+    i=0
+    for job in ${JOBS[@]}; do
+        wait $job
+        JOBS[$i]=''
+        i+=1
+    done
+}
+
+cleanup() {
+    echo "== Cleaning up jobs: ${JOBS[@]}"
+    for job in ${JOBS[@]}; do
+        kill $job
+    done
+}
+
+trap cleanup EXIT
+
 [ ! -d log ] && mkdir log || rm -f log/*.log
 
 build_image salt-base.dockerfile
 
-find $BUILD_PATH -name "*.dockerfile" | while read service; do
-    if [ "$service" == "salt-base.dockerfile" ]; then
+DOCKERFILES=$(find $BUILD_PATH -name "*.dockerfile")
+for service in ${DOCKERFILES[@]}; do
+    if [[ $service =~ *salt-base* ]]; then
         continue
     fi
 
-    if [ $JOBS -ge $MAX_JOBS ]; then
-        wait
-        JOBS=0
+    if [ ${#JOBS[@]} -ge $MAX_JOBS ]; then
+        wait_jobs
     fi
 
     build_image $service &
-    JOBS=$[ $JOBS + 1 ]
+    JOBS+=($!)
 done
 
-sleep 10
-wait
+wait_jobs
 echo
 
 for log_file in log/*.log; do
